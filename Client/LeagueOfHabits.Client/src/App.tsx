f598ad827b89b6d0ac6d00c54750359cb810ae5c
Lucas <lucas.g.oliveira1@hotmail.com>
2024-01-28 10:27:58 -0300

    feat: separete all month

--- Client/LeagueOfHabits.Client/src/App.tsx
+++ Client/LeagueOfHabits.Client/src/App.tsx
@@ -1,43 +1,66 @@
 // import { useState } from 'react'
-import './App.css'
+import './App.css';
 
-const RenderCheckBox = () => {
-
-  const week = Array.from({length: 35}, (_,index) => index + 1)
-  //TODO - continue daqui
-  const months = Array
-  const date = new Date(2024, 1, 0)
-  const inicialDate = date.getDay() + 1
-  const finalDate = new Date(2024, 2, 0).getDate() + inicialDate
+function MonthCheckBox({ monthNumber }: { monthNumber: number }) {
+  const week = Array.from({ length: 35 }, (_, index) => index + 1);
+  const date = new Date(2024, monthNumber, 0);
+  const inicialDate = date.getDay() + 1;
+  const finalDate = new Date(2024, monthNumber + 1, 0).getDate() + inicialDate;
 
   return (
     <>
-    <div className='checkbox-months'>
-      <div>
-        {week.map((_, index) => { 
-          
-          console.log(index < finalDate)
-          if(index < inicialDate){
-            return (<><input title={'formatDate'} className={'checkbox-hidden'} type="checkbox"/>{(index + 1) % 7 == 0 ? (<br></br>) : ' '}</>)
+      <div className="month">
+        {week.map((_, index) => {
+          console.log(index < finalDate);
+          if (index < inicialDate) {
+            return (
+              <>
+                <input
+                  title={'formatDate'}
+                  className={'checkbox-hidden'}
+                  type="checkbox"
+                />
+                {(index + 1) % 7 == 0 ? <br></br> : ' '}
+              </>
+            );
+          } else if (index < finalDate) {
+            return (
+              <>
+                <input
+                  title={'formatDate'}
+                  className="checkbox"
+                  type="checkbox"
+                />
+                {(index + 1) % 7 == 0 ? <br></br> : ' '}
+              </>
+            );
           }
-          else if(index < finalDate){
-              return (<><input title={'formatDate'} className='checkbox' type="checkbox"/>{(index + 1) % 7 == 0 ? (<br></br>) : ' '}</>)
-          }
-
         })}
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-function App() {
+const RenderCheckBox = () => {
+  const months = Array.from({ length: 12 }, (_, index) => index + 1);
 
   return (
     <>
-      <RenderCheckBox/>
+      <div className="checkbox-months">
+        {months.map((month) => (
+          <MonthCheckBox monthNumber={month - 1} />
+        ))}
+      </div>
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <>
+      <RenderCheckBox />
+    </>
+  );
 }
 
-export default App
+export default App;
